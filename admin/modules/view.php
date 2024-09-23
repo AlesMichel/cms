7708d3c07a1decd06d4416055a8770083c9ac9ef
Ales Michel <ales.michel@centrum.cz>
2024-09-23 18:17:36 +0200

    we can now view components data

--- admin/modules/view.php
+++ admin/modules/view.php
@@ -2,37 +2,67 @@
 //connect to db
 include("../../src/DbConnect/connect.php");
 include "../../src/Module/module.php";
+
+include("../templates/header.php");
+
 $db = \phpCms\DbConnect\connect::getInstance()->getConnection();
 $moduleName = $_GET["name"];
 $getTable = \phpCms\Module\module::findModuleByName($moduleName, $db);
 if ($moduleName) {
 
-    echo "GET module name =" . $moduleName . "</br>";
-    echo "GET table name =" . $getTable . "</br>";
+    //get module id by its name
+    $moduleId = \phpCms\Module\module::getModuleId($moduleName, $db);
+    // Fetch module component data using the static method
+    $moduleComponents = \phpCms\Module\module::getModuleData($moduleId, $db);
 
-    try {
+// Check if any components are found for this module
+    if ($moduleComponents === null || empty($moduleComponents)) {
+        echo "<p>No components found for this module.</p>";
+    } else {
+        echo "<h2>Components for Module ID: " . htmlspecialchars($moduleId) . " /  " . htmlspecialchars($moduleName) . " </h2>";
 
-        if ($getTable) {
-            echo "Module name: $getTable";
+// Start the HTML table
+        echo "<table class='table table-bordered'>";
+        echo "<thead>
+        <tr>
+            <th>ID in module_components</th>
+            <th>Component Instance</th>
+            <th>Component ID</th>
+            <th>Component Data</th>
+            <th>Actions</th>
+        </tr>
+      </thead>";
+        echo "<tbody>";
 
-            $sqlGetData = "SELECT * FROM `$getTable`";
-            $stmtGetData = $db->prepare($sqlGetData);
-            $stmtGetData->execute();
+// Loop through each component instance and display the components
+        foreach ($moduleComponents as $instance => $components) {
+            foreach ($components as $component) {
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($component['id']) . "</td>";
+                echo "<td>" . htmlspecialchars($instance) . "</td>";
+                echo "<td>" . htmlspecialchars($component['component_id']) . "</td>";
+                echo "<td>" . htmlspecialchars($component['component_data']) . "</td>";
 
-            $data = $stmtGetData->fetchAll(PDO::FETCH_ASSOC);
+                // Form and button inside a table cell
+                echo "<td>";
+                echo "<form method='GET' action='components/edit.php' class='d-inline'>";
+                echo "<input type='hidden' name='id' value='" . htmlspecialchars($component['id']) . "'>";
+                echo "<input type='hidden' name='module_id' value='" . htmlspecialchars($moduleId) . "'>";
+                echo "<input type='hidden' name='component_id' value='" . htmlspecialchars($component['component_id']) . "'>";
+                echo "<input type='hidden' name='instance' value='" . htmlspecialchars($instance) . "'>";
+                echo "<input type='hidden' name='component_data' value='" . htmlspecialchars($component['component_data']) . "'>";
+                echo "<button class='btn btn-primary btn-sm' type='submit'>Edit Data</button>";
+                echo "</form>";
+                echo "</td>";
 
-            foreach ($data as $row) {
-                echo "<div>";
-                echo "ID: " . $row['id'] . "<br>";
-                echo "Name: " . htmlspecialchars($row['moduleName']);
-                echo "</div><br>";
+                echo "</tr>";
             }
-        } else {
-
-            echo "Table $moduleName does not exist";
         }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
+
+        echo "</tbody>";
+        echo "</table>";
     }
 
+}else{
+    echo "Module table does not exist";
 }
