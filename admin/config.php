cc448dc9a7c20cf3d04f58de59916285b75bfef6
Ales Michel <105454105+AlesMichel@users.noreply.github.com>
2025-01-19 19:22:02 +0100

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -2,9 +2,9 @@
 const ABS_URL = "http://localhost/cms/admin";
 const ABS_PATH = __DIR__;
 
-const LOGIN = "admin";
-const PASS = "admin";
-const DBHOST = "127.0.0.1";
-const DBUSER = "root";
-const DBPASS = "";
-const DBNAME = "cms";
+const LOGIN = "xxxxxx";
+const PASS = "xxxxxxx";
+const DBHOST = "xxxxxxxxx";
+const DBUSER = "xxxxxx";
+const DBPASS = "xxxxxx";
+const DBNAME = "xxxxx";
